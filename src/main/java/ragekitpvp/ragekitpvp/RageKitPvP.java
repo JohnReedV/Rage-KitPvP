67077c51befaae659982a62875892b0fc080b66c
jjreedv <87283488+jjreedv@users.noreply.github.com>
2023-01-10 10:09:21 -0800

    make horses despawn and fix commands

--- src/main/java/ragekitpvp/ragekitpvp/RageKitPvP.java
+++ src/main/java/ragekitpvp/ragekitpvp/RageKitPvP.java
@@ -1,6 +1,8 @@
 package ragekitpvp.ragekitpvp;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.entity.*;
@@ -38,10 +40,6 @@ public class RageKitPvP extends JavaPlugin implements Listener {
     public void onEnable(){
         this.getServer().getPluginManager().registerEvents(this, this);
         inventory.createInv();
-        this.getCommand("head").setExecutor(new commands());
-        this.getCommand("kits").setExecutor(new commands());
-        this.getCommand("setspawn").setExecutor(new commands());
-        //this.getCommand("flex").setExecutor(new commands());
         System.out.println("GUI in");
     }
     @Override
@@ -49,6 +47,86 @@ public class RageKitPvP extends JavaPlugin implements Listener {
         System.out.println("GUI out");
     }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("head")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("players only BOSS");
+            }
+            if (sender.hasPermission("kits.head")) {
+                Player player = (Player) sender;
+                if (args.length == 0) {
+                    ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+                    SkullMeta meta = (SkullMeta) head.getItemMeta();
+                    meta.setOwner(player.getDisplayName());
+                    meta.setDisplayName(ChatColor.GREEN + player.getDisplayName() + "s" + ChatColor.AQUA + " head");
+                    head.setItemMeta(meta);
+                    player.getInventory().addItem(head);
+                    return true;
+                }
+                if (args.length > 0) {
+                    for (Player online : Bukkit.getOnlinePlayers()) {
+                        if (args[0].equalsIgnoreCase(online.getDisplayName())) {
+                            ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+                            SkullMeta meta = (SkullMeta) head.getItemMeta();
+                            meta.setOwner(online.getDisplayName());
+                            meta.setDisplayName(ChatColor.GREEN + online.getDisplayName() + "s" + ChatColor.AQUA + " head");
+                            head.setItemMeta(meta);
+                            player.getInventory().addItem(head);
+                            return true;
+                        }
+                    }
+                } else {
+                    player.sendMessage(ChatColor.GOLD + "Player not online");
+                }
+            }
+        }
+        if (label.equalsIgnoreCase("kits")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("players only BOSS");
+            }
+            if (sender.hasPermission("kits.open")) {
+                Player player = (Player) sender;
+                player.openInventory(inventory.inv);
+            }
+        }
+
+        if (label.equalsIgnoreCase("setspawn")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("players only BOSS");
+            }
+            if (sender.hasPermission("kits.setspawn")){
+                Player player = (Player) sender;
+                World world = player.getWorld();
+                Location loc = player.getLocation();
+                world.setSpawnLocation(loc.getBlockX(), loc.getBlockY() + 1, loc.getBlockZ());
+            }
+        }
+
+        if (label.equalsIgnoreCase("flex")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("players only BOSS");
+            } else {
+                Player player = (Player) sender;
+                Integer kills = player.getStatistic(Statistic.PLAYER_KILLS);
+                Integer deaths = player.getStatistic(Statistic.DEATHS);
+                Integer dmgDone = player.getStatistic(Statistic.DAMAGE_DEALT);
+                Integer dmgTaken = player.getStatistic(Statistic.DAMAGE_TAKEN);
+                Integer time = player.getStatistic(Statistic.TOTAL_WORLD_TIME);
+
+                getServer().dispatchCommand(getServer().getConsoleSender(), "say Here are "
+                        + player.getName() + "'s stats...");
+
+                getServer().dispatchCommand(getServer().getConsoleSender(),"say Died : " + deaths.toString());
+                getServer().dispatchCommand(getServer().getConsoleSender(),"say Killed : " + kills.toString());
+                getServer().dispatchCommand(getServer().getConsoleSender(),"say Damage done : " + dmgDone.toString());
+                getServer().dispatchCommand(getServer().getConsoleSender(),"say Damage Taken : " + dmgTaken.toString());
+                getServer().dispatchCommand(getServer().getConsoleSender(),"Time on server : " + time.toString());
+            }
+
+        }
+
+        return true;
+    }
 
     @EventHandler
     public void onClick(PlayerInteractEvent event) {
@@ -121,6 +199,8 @@ public class RageKitPvP extends JavaPlugin implements Listener {
                 if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
                     Horse h = (Horse) player.getWorld().spawnEntity(player.getLocation(), EntityType.HORSE);
                     h.setTamed(true);
+                    h.setCustomName("Steed O' " + player.getName());
+                    h.setCustomNameVisible(true);
                     h.setOwner(event.getPlayer());
                     h.getInventory().setSaddle(new ItemStack(Material.SADDLE, 1));
                     h.getInventory().setArmor(new ItemStack(Material.DIAMOND_HORSE_ARMOR));
@@ -178,9 +258,23 @@ public class RageKitPvP extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e) {
-        Player player = e.getPlayer();
-        e.setQuitMessage(ChatColor.GOLD + player.getName() + ChatColor.AQUA + " out lul");
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage(ChatColor.GOLD + player.getName() + ChatColor.AQUA + " out lul");
+
+        Collection<Entity> entities = player.getWorld().getEntities();
+        for (Entity ent : entities){
+            if (ent.getName().equalsIgnoreCase("Charles => " + player.getName())){
+                Warden charles = (Warden) ent;
+                charles.remove();
+                return;
+            }
+            if (ent.getName().equalsIgnoreCase("Steed O' " + player.getName())) {
+                Horse horse = (Horse) ent;
+                horse.remove();
+                return;
+            }
+        }
     }
 
     @EventHandler
@@ -435,6 +529,12 @@ public class RageKitPvP extends JavaPlugin implements Listener {
                 player.sendMessage(ChatColor.YELLOW + "you let charles die alone");
                 return;
             }
+            if (ent.getName().equalsIgnoreCase("Steed O' " + player.getName())) {
+                Horse horse = (Horse) ent;
+                horse.remove();
+                player.sendMessage(ChatColor.YELLOW + "horse gone king");
+                return;
+            }
         }
 
     }
@@ -445,8 +545,7 @@ public class RageKitPvP extends JavaPlugin implements Listener {
             entity.getDrops().clear();
         }
 
-        if (entity.getEntityType() == EntityType.WARDEN && entity.getEntity().getName().equalsIgnoreCase("Charles => "
-                + entity.getEntity().getKiller().getName())){
+        if (entity.getEntityType() == EntityType.WARDEN && entity.getEntity().getName().startsWith("Charles")){
             Location oldloc = entity.getEntity().getKiller().getLocation().clone();
             Location loc = oldloc;
 
