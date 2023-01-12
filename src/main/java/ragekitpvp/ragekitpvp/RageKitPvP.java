36714c3201aa60f972b60f9da4183b5104f38cbf
jjreedv <87283488+JohnReedV@users.noreply.github.com>
2023-01-11 18:05:54 -0800

    refactor more

--- src/main/java/ragekitpvp/ragekitpvp/RageKitPvP.java
+++ src/main/java/ragekitpvp/ragekitpvp/RageKitPvP.java
@@ -30,6 +30,7 @@ public class RageKitPvP extends JavaPlugin implements Listener {
     kitInv inventory = new kitInv();
     RageScoreboard scoreboard = new RageScoreboard();
     Items items = new Items();
+    Kits kits = new Kits();
 
     @Override
     public void onEnable(){
@@ -382,213 +383,21 @@ public class RageKitPvP extends JavaPlugin implements Listener {
         if (e.getSlot() == 0 || e.getSlot() == 9) {
             player.sendMessage(ChatColor.GOLD + "Not a kit");
         }
-        if (e.getSlot() == 1) {
-            player.getInventory().clear();
-            player.getInventory().addItem(items.chemW());
-            player.getInventory().addItem(items.pot1());
-            player.getInventory().addItem(items.pot2());
-            player.getInventory().addItem(items.pot6());
-            player.getInventory().addItem(items.pot3());
-            player.getInventory().addItem(items.pot4());
-            player.getInventory().addItem(items.pot5());
-            player.getInventory().setHelmet(items.chemHead());
-            player.getInventory().setChestplate(items.chemChest());
-            player.getInventory().setLeggings(items.chemLegs());
-            player.getInventory().setBoots(items.chemFeed());
-            player.closeInventory();
-        }
-        if (e.getSlot() == 2) {
-            player.getInventory().clear();
-            player.getInventory().addItem(items.getDoom());
-            player.getInventory().addItem(items.doomStick());
-            player.getInventory().setBoots(items.doomBoots());
-            player.closeInventory();
-        }
-        if (e.getSlot() == 3) {
-            player.getInventory().clear();
-            player.getInventory().setBoots(items.knightBoots());
-            player.getInventory().setLeggings(items.knightLegs());
-            player.getInventory().setChestplate(items.knightChest());
-            player.getInventory().setHelmet(items.knightHead());
-            player.getInventory().setItemInMainHand(items.knightSword());
-            player.getInventory().setItemInOffHand(items.knightSheild());
-            player.closeInventory();
-        }
-        if (e.getSlot() == 4) {
-            player.getInventory().clear();
-            player.getInventory().addItem(items.archerBow());
-            player.getInventory().addItem(items.archerArrow());
-            player.getInventory().setBoots(items.archerFoot());
-            player.getInventory().setHelmet(items.archerHead());
-            player.closeInventory();
-        }
-        if (e.getSlot() == 5) {
-            player.getInventory().clear();
-            player.getInventory().addItem(items.pyroSword());
-            player.getInventory().addItem(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE));
-            player.getInventory().setBoots(items.pyroBoots());
-            player.getInventory().setLeggings(items.pyroLegs());
-            player.getInventory().setChestplate(items.pyroChest());
-            player.getInventory().setHelmet(items.pyroHelm());
-            player.closeInventory();
-        }
-        if (e.getSlot() == 6) {
-            player.getInventory().clear();
-            player.getInventory().addItem(items.invStick());
-            player.closeInventory();
-        }
-        if (e.getSlot() == 10) {
-            if (2 > 1) {
-                player.getInventory().clear();
-                player.getInventory().setBoots(items.horseFeet());
-                player.getInventory().setLeggings(items.horseLegs());
-                player.getInventory().setChestplate(items.horseChest());
-                player.getInventory().setHelmet(items.horseHead());
-                player.getInventory().addItem(items.horseW());
-                player.getInventory().addItem(items.horseSpawn());
-                player.closeInventory();
-
-            }
-            else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
-        if (e.getSlot() == 11) {
-            if (2 > 1) {
-                player.getInventory().clear();
-                player.getInventory().addItem(new ItemStack(Material.NETHERITE_SWORD));
-                player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 64));
-                player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 64));
-                player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 64));
-                player.getInventory().setHelmet(new ItemStack(Material.DRAGON_HEAD));
-                player.getInventory().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
-                player.getInventory().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
-                player.getInventory().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
-                player.closeInventory();
-            } else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
-        if (e.getSlot() == 7) {
-            player.getInventory().clear();
-            ItemStack LDHelmet = new ItemStack(Material.NETHERITE_SWORD);
-            LDHelmet.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 20);
-            player.getInventory().addItem(new ItemStack[]{new ItemStack(LDHelmet)});
-            player.getInventory().setHelmet(new ItemStack(Material.NETHERITE_HELMET));
-            player.getInventory().setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
-            player.getInventory().setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
-            player.getInventory().setBoots(new ItemStack(Material.NETHERITE_BOOTS));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2147483647, 3));
-            player.closeInventory();
-        }
-        if (e.getSlot() == 8) {
-            player.getInventory().clear();
-            player.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
-            player.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
-            player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
-            player.getInventory().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
-            getServer().dispatchCommand(getServer().getConsoleSender(), "shot give "
-                    + player.getName() + " python");
-            getServer().dispatchCommand(getServer().getConsoleSender(), "shot give "
-                    + player.getName() + " flashbang 3");
-            player.closeInventory();
-        }
-        if (e.getSlot() == 12) {
-            if (2 > 1) {
-                player.getInventory().clear();
-                player.getEquipment().setHelmet(items.cactusHead());
-                player.getEquipment().setChestplate(items.cactusChest());
-                player.getEquipment().setLeggings(items.cactusLeg());
-                player.getEquipment().setBoots(items.cactusFoot());
-                player.closeInventory();
-            }
-            else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
-        if (e.getSlot() == 13) {
-            if (2 > 1) {
-                player.getInventory().clear();
-                ItemStack helmet111 = new ItemStack(Material.LEATHER_HELMET);
-                helmet111.addUnsafeEnchantment(Enchantment.DURABILITY, 150);
-                helmet111.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
-                player.getEquipment().setHelmet(helmet111);
-                ItemStack capacete0 = new ItemStack(Material.LEATHER_CHESTPLATE);
-                capacete0.addUnsafeEnchantment(Enchantment.DURABILITY, 150);
-                capacete0.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
-                player.getEquipment().setChestplate(capacete0);
-                ItemStack peitoral0 = new ItemStack(Material.LEATHER_LEGGINGS);
-                peitoral0.addUnsafeEnchantment(Enchantment.DURABILITY, 150);
-                peitoral0.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
-                player.getEquipment().setLeggings(peitoral0);
-                ItemStack calca0 = new ItemStack(Material.LEATHER_BOOTS);
-                calca0.addUnsafeEnchantment(Enchantment.DURABILITY, 150);
-                calca0.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
-                player.getEquipment().setBoots(calca0);
-                getServer().dispatchCommand(getServer().getConsoleSender(), "shot give "
-                        + player.getName() + " AK-47");
-                getServer().dispatchCommand(getServer().getConsoleSender(), "shot give "
-                        + player.getName() + " cocopops 5");
-                getServer().dispatchCommand(getServer().getConsoleSender(), "shot give "
-                        + player.getName() + " toaster");
-                player.closeInventory();
-            }
-            else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
-
-        if (e.getSlot() == 14) {
-            if (2 > 1){
-                player.getInventory().clear();
-                player.getInventory().addItem(items.wardenFlint());
-                player.getInventory().setChestplate(items.wardenElytra());
-                player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 12000, 3));
-                player.closeInventory();
-
-            } else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
-
-        if (e.getSlot() == 15) {
-            if (2 > 1){
-                player.getInventory().clear();
-                player.getInventory().addItem(items.aquaTrident());
-                player.getInventory().addItem(items.aquaHeart());
-                player.getInventory().setHelmet(items.aquaHead());
-                player.getInventory().setBoots(items.aquaFoot());
-                player.closeInventory();
-
-            } else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
-
-        if (e.getSlot() == 16) {
-            if (2 > 1){
-                player.getInventory().clear();
-                player.getInventory().setHelmet(items.sniperHead());
-                player.getInventory().setBoots(items.sniperFoots());
-                player.getInventory().setLeggings(items.sniperLegs());
-                player.getInventory().setChestplate(items.sniperChest());
-                getServer().dispatchCommand(getServer().getConsoleSender(),
-                        "shot give " + player.getName() + " hunting");
-                getServer().dispatchCommand(getServer().getConsoleSender(),
-                        "shot give " + player.getName() + " airstrike");
-                player.closeInventory();
-
-            } else {
-                player.sendMessage(ChatColor.GOLD + "You are not " + ChatColor.UNDERLINE + "" + ChatColor.BOLD
-                        + "" + ChatColor.GREEN + "VIP");
-            }
-        }
+        if (e.getSlot() == 1) { kits.getChemist(player); }
+        if (e.getSlot() == 2) { kits.getDoom(player); }
+        if (e.getSlot() == 3) { kits.getKnight(player); }
+        if (e.getSlot() == 4) { kits.getArcher(player); }
+        if (e.getSlot() == 5) { kits.getPyro(player); }
+        if (e.getSlot() == 6) { kits.getNinja(player); }
+        if (e.getSlot() == 7) { kits.getTank(player); }
+        if (e.getSlot() == 8) { kits.getOutcast(player); }
+        if (e.getSlot() == 10) { kits.getJockey(player); }
+        if (e.getSlot() == 11) { kits.getEnderman(player); }
+        if (e.getSlot() == 12) { kits.getCactus(player); }
+        if (e.getSlot() == 13) { kits.getTerrorist(player); }
+        if (e.getSlot() == 14) { kits.getWarton(player); }
+        if (e.getSlot() == 15) { kits.getAquaman(player); }
+        if (e.getSlot() == 16) { kits.getSniper(player); }
     }
 
     @EventHandler
